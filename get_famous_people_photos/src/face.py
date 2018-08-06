1cc2ecb1986cfe5a3dc87ce4578638c8a85baf2f
Arman Rahman <arrahm@microsoft.com>
2018-08-06 00:28:07 -0400

    fixes and optimization of multiprocessing

--- get_famous_people_photos/src/face.py
+++ get_famous_people_photos/src/face.py
@@ -40,6 +40,7 @@ import numpy as np
 import tensorflow as tf
 from facenet_sandberg import facenet, validate_on_lfw
 from facenet_sandberg.align import align_dataset_mtcnn, detect_face
+from memory_profiler import profile
 from scipy import misc
 
 facenet_model_checkpoint = "../../common/models/20180402-114759.pb"
@@ -47,8 +48,7 @@ debug = False
 
 
 class Face:
-    """
-    Class representing a single face
+    """Class representing a single face
     """
     def __init__(self):
         self.name = None
@@ -57,11 +57,11 @@ class Face:
         self.container_image = None
         self.embedding = None
         self.matches = []
+        self.url = None
 
 
 class Match:
-    """
-    Class representing a single face
+    """Class representing a match between two faces
     """
     def __init__(self):
         self.face_1 = Face()
@@ -71,32 +71,83 @@ class Match:
 
 
 class Identifier:
+    """Class to detect, encode, and match faces
+
+    Arguments:
+        threshold {Float} -- Distance threshold to determine matches
+    """
     def __init__(self, threshold=1.10):
         self.detector = Detector()
         self.encoder = Encoder()
         self.threshold = threshold
     
-    def detect_encode(self, image):
-        faces = self.detector.find_faces(image)
+    def download_image(self, url):
+        """Downloads an image from the url as a cv2 image
+        
+        Arguments:
+            url {str} -- url of image
+        
+        Returns:
+            cv2 image -- image array
+        """
+
+        req = urlopen(url)
+        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+        image = cv2.imdecode(arr, -1)
+        return image
+    
+    def detect_encode(self, image, face_limit=5):
+        """Detects faces in an image and encodes them
+        
+        Arguments:
+            image {cv2 image (np array)} -- image to find faces and encode
+            face_limit {int} -- Maximum # of faces allowed in image. 
+                                If over limit returns empty list 
+
+        Returns:
+            Face[] -- list of Face objects with embeddings attached
+        """
+
+        faces = self.detector.find_faces(image, face_limit)
         for face in faces:
             face.embedding = self.encoder.generate_embedding(face)
         return faces
     
-    def compare_faces(self, image_1, image_2, is_from_url):
+    def detect_encode_all(self, images, urls=None, save_memory=False):
+        """For a list of images finds and encodes all faces
+        
+        Arguments:
+            images {List or iterable of cv2 images} -- images to encode
+        
+        Keyword Arguments:
+            urls {str[]} -- Optional list of urls to attach to Face objects. 
+                            Should be same length as images if used. (default: {None})
+            save_memory {bool} -- Saves memory by deleting image array from Face objects.
+                                  Should only be used if with r(default: {False})
+        
+        Returns:
+            [type] -- [description]
+        """
+
+        all_faces = self.detector.bulk_find_face(images, urls)
+        all_embeddings = self.encoder.get_all_embeddings(all_faces, save_memory)
+        return all_embeddings
+    
+    def compare_embedding(self, embedding_1, embedding_2):
+        distance = facenet.distance(embedding_1.reshape(1,-1), embedding_2.reshape(1,-1), distance_metric=0)[0]
+        is_match = False
+        if distance < self.threshold:
+            is_match = True
+        return is_match, distance
+
+    def compare_images(self, image_1, image_2):
         match = Match()
-        if is_from_url:
-            req = urlopen(image_1)
-            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-            image_1 = cv2.imdecode(arr, -1)
-            req = urlopen(image_2)
-            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-            image_2 = cv2.imdecode(arr, -1)
         image_1_faces = self.detect_encode(image_1)
         image_2_faces = self.detect_encode(image_2)
         if image_1_faces and image_2_faces:
             for face_1 in image_1_faces:
                 for face_2 in image_2_faces:
-                    distance = np.sqrt(np.sum(np.square(np.subtract(face_1.embedding, face_2.embedding))))
+                    distance = facenet.distance(face_1.embedding.reshape(1,-1), face_2.embedding.reshape(1,-1), distance_metric=0)[0]
                     if distance < match.score:
                         match.score = distance
                         match.face_1 = face_1
@@ -107,22 +158,21 @@ class Identifier:
     
     def find_all_matches(self, image_directory):
         all_images = glob(image_directory + '/*')
-        all_faces = []
         all_matches = []
-        for image in all_images:
-            all_faces += self.detect_encode(image)
+        all_faces = self.detect_encode_all(all_images)
+        # Really inefficient way to check all combinations
         for face_1, face_2 in itertools.combinations(all_faces, 2):
-            match = Match()
-            match.face_1 = face_1
-            match.face_2 = face_2
-            match.score = np.sqrt(np.sum(np.square(np.subtract(face_1.embedding, face_2.embedding))))
-            if match.score < self.threshold:
+            is_match, score = self.compare_embedding(face_1.embedding, face_2.embedding)
+            if is_match:
+                match = Match()
+                match.face_1 = face_1
+                match.face_2 = face_2
                 match.is_match = True 
+                match.score = score
                 all_matches.append(match)
                 face_1.matches.append(match)
                 face_2.matches.append(match)
         return all_faces, all_matches
-
 
 class Encoder:
     def __init__(self):
@@ -141,17 +191,30 @@ class Encoder:
         feed_dict = {self.images_placeholder: [prewhiten_face], self.phase_train_placeholder: False}
         return self.sess.run(self.embeddings, feed_dict=feed_dict)[0]
     
+    def get_all_embeddings(self, all_faces, save_memory=False):
+        all_images = [facenet.prewhiten(face.image) for face in all_faces]
+
+        # Run forward pass to calculate embeddings
+        feed_dict = {self.images_placeholder: all_images, self.phase_train_placeholder: False}
+        embed_array = self.sess.run(self.embeddings, feed_dict=feed_dict)
+        
+        for index, face in enumerate(all_faces):
+            if save_memory:
+                face.image = None
+            face.embedding = embed_array[index]
+        return all_faces
 
 
 class Detector:
     # face detection parameters
-    def __init__(self, face_crop_size=160, face_crop_margin=32, gpu_memory_fraction=0.4):
+    def __init__(self, face_crop_size=160, face_crop_margin=32, gpu_memory_fraction=0.4, detect_multiple_faces=True):
         self.pnet, self.rnet, self.onet = self._setup_mtcnn(gpu_memory_fraction)
         self.face_crop_size = face_crop_size
         self.face_crop_margin = face_crop_margin
         self.minsize = 20  # minimum size of face
         self.threshold = [0.6, 0.7, 0.7]  # three steps's threshold
         self.factor = 0.709  # scale factor
+        self.detect_multiple_faces = detect_multiple_faces
 
     def _setup_mtcnn(self, gpu_memory_fraction):
         with tf.Graph().as_default():
@@ -159,30 +222,68 @@ class Detector:
             sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
             with sess.as_default():
                 return detect_face.create_mtcnn(sess, None)
+    
+    def get_face_from_bb(self, bounding_boxes, img, min_size=50):
+        nrof_faces = bounding_boxes.shape[0]
+        faces = []
+        if nrof_faces>0:
+            det = bounding_boxes[:,0:4]
+            det_arr = []
+            img_size = np.asarray(img.shape)[0:2]
+            if nrof_faces>1:
+                if self.detect_multiple_faces:
+                    for i in range(nrof_faces):
+                        det_arr.append(np.squeeze(det[i]))
+                else:
+                    bounding_box_size = (det[:,2]-det[:,0])*(det[:,3]-det[:,1])
+                    img_center = img_size / 2
+                    offsets = np.vstack([ (det[:,0]+det[:,2])/2-img_center[1], (det[:,1]+det[:,3])/2-img_center[0] ])
+                    offset_dist_squared = np.sum(np.power(offsets,2.0),0)
+                    index = np.argmax(bounding_box_size-offset_dist_squared*2.0) # some extra weight on the centering
+                    det_arr.append(det[index,:])
+            else:
+                det_arr.append(np.squeeze(det))
+            for i, det in enumerate(det_arr):
+                det = np.squeeze(det)
+                bb = np.zeros(4, dtype=np.int32)
+                bb[0] = np.maximum(det[0]-self.face_crop_margin/2, 0)
+                bb[1] = np.maximum(det[1]-self.face_crop_margin/2, 0)
+                bb[2] = np.minimum(det[2]+self.face_crop_margin/2, img_size[1])
+                bb[3] = np.minimum(det[3]+self.face_crop_margin/2, img_size[0])
+                cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                if cropped.shape[0] > min_size and cropped.shape[1] > min_size:
+                    face = Face()
+                    face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
+                    faces.append(face)
+        return faces
 
-    def find_faces(self, image):
+    def bulk_find_face(self, images, urls=None, face_limit=5):
+        all_faces = []
+        for index, image in enumerate(images):
+            bb, _ = detect_face.detect_face(image, self.minsize,
+                                        self.pnet, self.rnet, self.onet,
+                                        self.threshold, self.factor)
+            if urls and index < len(urls):
+                faces = self.get_face_from_bb(bb, image)
+                if len(faces) < face_limit:
+                    for face in faces:
+                        face.url = urls[index]
+                        all_faces.append(face)
+            else:
+                faces = self.get_face_from_bb(bb, image)
+                all_faces.append(faces)
+        return all_faces 
+    
+    def find_faces(self, image, face_limit=5):
         if type(image) == str:
             image = cv2.imread(image)
         faces = []
         bounding_boxes, _ = detect_face.detect_face(image, self.minsize,
                                                            self.pnet, self.rnet, self.onet,
                                                            self.threshold, self.factor)
-        for bb in bounding_boxes:
-            face = Face()
-            face.container_image = image
-            face.bounding_box = np.zeros(4, dtype=np.int32)
-
-            img_size = np.asarray(image.shape)[0:2]
-            face.bounding_box[0] = np.maximum(bb[0] - self.face_crop_margin / 2, 0)
-            face.bounding_box[1] = np.maximum(bb[1] - self.face_crop_margin / 2, 0)
-            face.bounding_box[2] = np.minimum(bb[2] + self.face_crop_margin / 2, img_size[1])
-            face.bounding_box[3] = np.minimum(bb[3] + self.face_crop_margin / 2, img_size[0])
-            cropped = image[face.bounding_box[1]:face.bounding_box[3], face.bounding_box[0]:face.bounding_box[2], :]
-            face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
-            faces.append(face)
-        if debug:
-            for i, face in enumerate(faces):
-                cv2.imshow("Face: " + str(i), face.image)
+        faces = self.get_face_from_bb(bounding_boxes, image)
+        if len(faces) > face_limit:
+            return []
         return faces
 
 
--- get_famous_people_photos/src/get_famous_people_photos.py
+++ get_famous_people_photos/src/get_famous_people_photos.py
@@ -1,30 +1,38 @@
 import collections
 import itertools
 import os
+import pickle
+import time
 from ctypes import c_int
 from functools import wraps
 from io import BytesIO
-from multiprocessing import Lock, Pool, Value
+from itertools import islice
+from multiprocessing import Lock, Manager, Pool, Queue, Value
 from multiprocessing.dummy import Pool as ThreadPool
 from os import getenv
 from time import time
 
 import cv2
 import dhash
+import progressbar as pb
 import pybktree
 import requests
+import tensorflow as tf
+from memory_profiler import profile
+from pathos.multiprocessing import ProcessPool
 from PIL import Image
 from requests import exceptions
 
 import face
 
-BING_API_KEY = getenv('BING_API_KEY', '')
-NUM_THREADS = getenv('NUM_THREADS', 100)
-NUM_PROCESSES = getenv('NUM_PROCESSES', 8)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+BING_API_KEY = str(getenv('BING_API_KEY', 'c8e183c6cf57419bb0c0ee885b76bbe5'))
+NUM_THREADS = int(getenv('NUM_THREADS', 50))
+NUM_PROCESSES = min(int(getenv('NUM_PROCESSES', 4)), os.cpu_count())
 MAX_RESULTS = 150
 
 URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
-HEADERS = {"Ocp-Apim-Subscription-Key" : str(BING_API_KEY)}
+HEADERS = {"Ocp-Apim-Subscription-Key" : BING_API_KEY}
 
 EXCEPTIONS = set([IOError, FileNotFoundError,
 	exceptions.RequestException, exceptions.HTTPError,
@@ -35,34 +43,44 @@ HASH_URL = collections.namedtuple('HASH_URL', 'bits url')
 counter = Value(c_int)  # defaults to 0
 counter_lock = Lock()
 
-TOTAL_COUNT = 0
+widgets_urls = ['Fetching urls: ', pb.Percentage(), ' ', 
+            pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
+widgets_pare = ['Pare and Download urls: ', pb.Percentage(), ' ', 
+            pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
+timer = None
+
 
 def increment():
-    with counter_lock:
-        counter.value += 1
+	with counter_lock:
+		counter.value += 1
 
 
-def timing(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        start = time()
-        result = f(*args, **kwargs)
-        end = time()
-        print('Elapsed time: {}'.format(end-start))
-        return result
-    return wrapper
+def reset():
+	global timer
+	with counter_lock:
+		counter.value == 0
+	timer = None
 
 
 def url_to_image(url):
-	image_data = requests.get(url)
-	image_data.raise_for_status()
-	image = Image.open(BytesIO(image_data.content))
+	try:
+		image_data = requests.get(url)
+		image_data.raise_for_status()
+		image = Image.open(BytesIO(image_data.content))
+	except Exception as e:
+		print(e)
+		image = None
+		time.sleep(120)
 	return image
 
 
 def url_to_img_hash(url):
-	image = url_to_image(url)
-	image_hash = dhash.dhash_int(image)
+	try:
+		image = url_to_image(url)
+		image_hash = dhash.dhash_int(image)
+	except Exception as e:
+		print(e)
+		image_hash = None
 	return image_hash
 
 
@@ -71,35 +89,56 @@ def image_distance(x, y):
 
 
 def get_photos(famous_people_file):
-	global TOTAL_COUNT
+	global timer
 	people = []
 	with open(famous_people_file) as fp:
 		people = fp.read().splitlines() 
 	unique_people = set(people)
-	TOTAL_COUNT = len(unique_people)
-	urls_and_people = urls_thread(unique_people)
-	pare_multi_process(urls_and_people)
+	total_count = len(unique_people)
+
+	# Check if we have cached our image urls
+	url_file = '../../common/text_files/image_urls.data'
+	if not os.path.isfile(url_file):
+		timer = pb.ProgressBar(widgets=widgets_urls, maxval=total_count).start()
+		threaded_result = urls_thread(unique_people)
+		urls_and_people = [_ for _ in threaded_result]
+		with open(url_file, 'wb') as filehandle:  
+			pickle.dump(urls_and_people, filehandle)
+	else:
+		with open(url_file, 'rb') as filehandle:  
+			# read the data as binary data stream
+			urls_and_people = pickle.load(filehandle)
+	reset()
+	
+	timer = pb.ProgressBar(widgets=widgets_pare, maxval=total_count).start()
+	pare_multi_process(urls_and_people[:12])
 
 
-@timing
 def urls_thread(unique_people):
-	print("[INFO] Fetching image urls")
-	thread_pool = ThreadPool(int(NUM_THREADS)) 
-	urls_and_people = thread_pool.map(get_urls, unique_people)
+	print("[INFO] Fetching image urls with {} threads".format(NUM_THREADS))
+	thread_pool = ThreadPool(NUM_THREADS) 
+	urls_and_people = thread_pool.imap(safe_get_urls, unique_people)
 	thread_pool.close() 
 	thread_pool.join() 
 	print("[INFO] Done fetching image urls")
 	return urls_and_people
 
 
-@timing
 def pare_multi_process(urls_and_people):
-	print("[INFO] Paring and downlaoding all image urls")
-	pare_pool = Pool(int(NUM_PROCESSES))
-	pare_pool.map(pare_matches_and_download, urls_and_people)
+	print("[INFO] Paring and downlaoding all image urls with {} processes".format(NUM_PROCESSES))
+	urls, person = zip(*urls_and_people)
+	pare_pool = ProcessPool(NUM_PROCESSES)
+	pare_pool.imap(safe_pare_matches_and_download, urls, person)
 	pare_pool.close() 
 	pare_pool.join()
 	print("[INFO] Done paring and downlaoding all image urls")
+
+
+def safe_get_urls(*args, **kwargs):
+	try:
+		return get_urls(*args, **kwargs)
+	except Exception as e:
+		print(e)
 
 
 def get_urls(person):
@@ -108,56 +147,83 @@ def get_urls(person):
 	search.raise_for_status()
 	results = search.json()
 	thumbnail_urls = [img["thumbnailUrl"] + '&c=7&w=250&h=250' for img in results["value"]]
+	try:
+		# update counter 
+		increment()
+		timer.update(int(counter.value))
+	except Exception as e:
+		print(e)
 	return thumbnail_urls, person
 
 
-@timing
-def pare_matches_and_download(urls_and_person):
-	thumbnail_urls, person = urls_and_person
-	# print("[INFO] paring urls for '{}'".format(person))
-	urls = []
-	# Make sure all the matches are of the same person
-	identifier = face.Identifier()
-	if len(thumbnail_urls) > 1:
-		for image_url in thumbnail_urls:
-			match = identifier.compare_faces(thumbnail_urls[0], image_url, True)
-			if match.is_match:
-				urls.append(image_url)
-	
-	# Make sure there are no duplicate images
-	image_hashes = [HASH_URL(url_to_img_hash(url), url) for url in urls]
-	tree = pybktree.BKTree(image_distance, image_hashes)
-	to_discard = []
-	urls_to_keep = []
-	for image_hash in image_hashes:
-		if image_hash not in to_discard:
-			matches = tree.find(image_hash, 3)
-			for match in matches:
-				if match[1].url != image_hash.url:
-					to_discard.append(match[1])
-			urls_to_keep.append(image_hash.url)
-	
-	# Download the images 
-	download_urls(person, urls_to_keep)
+def safe_pare_matches_and_download(*args, **kwargs):
+	try:
+		return pare_matches_and_download(*args, **kwargs)
+	except Exception as e:
+		print(e)
 
-	# update counter 
-	increment()
-	if int(counter.value)%(TOTAL_COUNT//1000) == 0:
-		percent = int(round(counter.value/TOTAL_COUNT, 2) * 100)
-		print('{}% Complete'.format(str(percent)))
+
+def pare_matches_and_download(thumbnail_urls, person):
+	urls = set()
+	directory = '../../common/images/' + person
+	if not os.path.exists(directory) and len(thumbnail_urls) > 10:
+		# Make sure all the matches are of the same person
+		try:
+			identifier = face.Identifier(threshold=1.0)
+			images = map(identifier.download_image, thumbnail_urls)
+			urls_and_embeddings = identifier.detect_encode_all(images, thumbnail_urls, True)
+			anchor_embedding = urls_and_embeddings[0].embedding
+			# Assume first image is of the right person and check other images are of the same person
+			for other in urls_and_embeddings:
+				is_match, distance = identifier.compare_embedding(anchor_embedding, other.embedding)
+				# print('dist: {} between {} and {}'.format(distance,urls_and_embeddings[0].url, other.url))
+				if is_match:
+					urls.add(other.url)
+			del identifier
+		except Exception as e:
+			print(e)
+		
+		# Make sure there are no duplicate images
+		image_hashes = [HASH_URL(url_to_img_hash(url), url) for url in urls]
+		tree = pybktree.BKTree(image_distance, image_hashes)
+		# this makes images saved in order of similarity so we can spot duplicates easier
+		sorted_image_hashes = sorted(tree)
+		to_discard = []
+		urls_to_keep = set()
+		for image_hash in sorted_image_hashes:
+			if image_hash not in to_discard:
+				# gets pictures within a hamming distance of 3
+				matches = tree.find(image_hash, 3)
+				for match in matches:
+					if match[1].url != image_hash.url:
+						to_discard.append(match[1])
+				urls_to_keep.add(image_hash.url)
+		
+		# Download the images 
+		download_urls(person, list(urls_to_keep))
+
+	# Update counter 
+	try:
+		increment()
+		timer.update(int(counter.value))
+	except Exception as e:
+		print(e)
 
 
 def download_urls(person, urls):
 	if len(urls) > 5:
-		directory = '../../common/images/' + person
-		if not os.path.exists(directory):
-			os.makedirs(directory)
-		count = 0
-		for url in urls:
-			image = url_to_image(url)
-			image_path = os.path.join(directory, str(count))
-			image.save(image_path + '.jpg')
-			count += 1
+		try:
+			directory = '../../common/images/' + person
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			count = 0
+			for url in urls:
+				image = url_to_image(url)
+				image_path = os.path.join(directory, str(count))
+				image.save(image_path + '.jpg')
+				count += 1
+		except Exception as e:
+			print(e)
 
 
 if __name__ == "__main__":
